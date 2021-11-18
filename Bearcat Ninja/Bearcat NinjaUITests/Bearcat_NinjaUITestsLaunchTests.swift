d752be64ae3a700a281397af1ec0f0970c085420
Berk Cohadar <berkcohadar1@gmail.com>
2021-11-18 10:51:33 -0500

    Project files uploaded.

---
+++ Bearcat Ninja/Bearcat NinjaUITests/Bearcat_NinjaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Bearcat_NinjaUITestsLaunchTests.swift
+//  Bearcat NinjaUITests
+//
+//  Created by Berk Çohadar on 11/18/21.
+//
+
+import XCTest
+
+class Bearcat_NinjaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
