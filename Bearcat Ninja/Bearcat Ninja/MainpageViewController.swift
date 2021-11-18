5817ab8100e1768ef24093646859fa49a18ee361
Berk Cohadar <berkcohadar1@gmail.com>
2021-11-18 11:02:05 -0500

    Second page added. Nav controller added.

---
+++ Bearcat Ninja/Bearcat Ninja/MainpageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainpageViewController.swift
+//  Bearcat Ninja
+//
+//  Created by Berk Çohadar on 11/18/21.
+//
+
+import UIKit
+
+class MainpageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
