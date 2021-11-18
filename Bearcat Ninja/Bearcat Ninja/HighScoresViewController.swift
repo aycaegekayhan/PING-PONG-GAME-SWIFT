eac860899c5320f0f4ac4034a640e2878fcbe90e
Berk Cohadar <berkcohadar1@gmail.com>
2021-11-18 11:13:09 -0500

    All screens and view controllers added.

---
+++ Bearcat Ninja/Bearcat Ninja/HighScoresViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HighScoresViewController.swift
+//  Bearcat Ninja
+//
+//  Created by Berk Çohadar on 11/18/21.
+//
+
+import UIKit
+
+class HighScoresViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
---
+++ Bearcat Ninja/Bearcat Ninja/SettingsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsViewController.swift
+//  Bearcat Ninja
+//
+//  Created by Berk Çohadar on 11/18/21.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
