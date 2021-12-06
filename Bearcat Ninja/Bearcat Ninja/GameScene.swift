9eba0cb8a0190a40f44bce55e517c9f222b23920
Berk Cohadar <berkcohadar1@gmail.com>
2021-12-05 20:54:48 -0500

    Game scene updated. Background added. Player added. Obstacles added.

--- Bearcat Ninja/Bearcat Ninja/GameScene.swift
+++ Bearcat Ninja/Bearcat Ninja/GameScene.swift
@@ -7,35 +7,104 @@
 
 import SpriteKit
 import GameplayKit
+import AVFoundation
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
+    private var obstacle: SKShapeNode!
+    // initialize obstacle array
+    
+    var baseGround: SKShapeNode!
+    
+    var flag = false
+    var nodeCount = 0
+    
+    var timer = Timer()
+    var time = 1
+    
+    var initLocation = CGPoint(x: 0, y: 0)
     
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        let background = SKSpriteNode(imageNamed: "3")
+        background.position = CGPoint(x: frame.midX, y: frame.midY)
+        background.zPosition = 0
+        background.physicsBody?.friction = 0
+        self.addChild(background)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        let initLocation = CGPoint(x: CGFloat(frame.midX), y: CGFloat(-self.frame.maxY))
+        let baseSize = CGSize(width: self.frame.width, height: 120.0)
+        baseGround = SKShapeNode(rectOf: baseSize, cornerRadius: CGFloat(10))
+        baseGround.position = initLocation
+        baseGround.fillColor = .systemGreen
+        baseGround.physicsBody = SKPhysicsBody(rectangleOf: baseSize)
+        baseGround.physicsBody!.affectedByGravity = false
+        baseGround.physicsBody?.isDynamic = false
+        baseGround.physicsBody?.collisionBitMask = 2
+        baseGround.physicsBody?.categoryBitMask = 1
+        baseGround.physicsBody?.angularDamping = 0
+        baseGround.physicsBody?.linearDamping = 0
+        baseGround.physicsBody?.friction = 0
+        baseGround.physicsBody?.restitution = 0
+        baseGround.physicsBody?.contactTestBitMask = 2
+        baseGround.name = "baseGround"
+        baseGround.zPosition = 2
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        let border = SKPhysicsBody(edgeLoopFrom: (view.scene?.frame)!)
+        border.friction = 0
+        self.physicsBody = border
+        self.physicsBody?.contactTestBitMask = 2
+        self.physicsWorld.contactDelegate = self
+        self.name = "gameArea"
+        
+        self.addChild(baseGround)
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(GameScene.createObstacle), userInfo: nil, repeats: true)
+        
     }
     
+    @objc func createObstacle() {
+        time = time - 1
+        if (time == 0){
+            
+            
+            if (flag) {
+                // RIGHT OBSTACLE
+                initLocation = CGPoint(x: 250, y: CGFloat(frame.midY))
+                //obstacle.fillColor = .systemMint
+                flag = false
+
+            } else {
+                // LEFT OBSTACLE
+                initLocation = CGPoint(x: -250, y: CGFloat(frame.midY))
+                //obstacle.fillColor = .systemBrown
+                flag = true
+            }
+            
+            time = 1
+            
+            nodeCount+=1
+            
+            let obstacleSize = CGSize(width: 150, height: 40.0)
+            obstacle = SKShapeNode(rectOf: obstacleSize, cornerRadius: CGFloat(10))
+            obstacle.name = "obstacle"+String(nodeCount)
+            obstacle.position = initLocation
+            obstacle.zPosition = 1
+            obstacle.fillColor = .systemMint
+            
+            
+            obstacle.physicsBody = SKPhysicsBody(rectangleOf: obstacleSize)
+            obstacle.physicsBody?.affectedByGravity = false
+            obstacle.physicsBody?.collisionBitMask = 2
+            obstacle.physicsBody?.categoryBitMask = 1
+            obstacle.physicsBody?.contactTestBitMask = 2
+            
+            self.addChild(obstacle)
+            obstacle.physicsBody?.applyImpulse(CGVector(dx: 0, dy: -50))
+
+        }
+    }
     
     func touchDown(atPoint pos : CGPoint) {
         if let n = self.spinnyNode?.copy() as! SKShapeNode? {
